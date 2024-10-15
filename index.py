570ff46fed9de0ab848dbc97fb5b30bbfc4d9221
Mohammad Hossein Dalghi <mamasi_dl@yahoo.com>
2024-10-15 09:45:22 +0330

    Add libraries

---
+++ index.py
@@ -0,0 +1,7 @@
+# Importing libraries
+import numpy as np
+import os
+import cv2
+import matplotlib.pyplot as plt
+from sklearn import linear_model
+from sklearn.model_selection import train_test_split
