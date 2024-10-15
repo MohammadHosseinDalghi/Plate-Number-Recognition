b92944bb127fa3efd2e91368148952197fb38744
Mohammad Hossein Dalghi <mamasi_dl@yahoo.com>
2024-10-15 10:10:32 +0330

    Add standard variables

--- index.py
+++ index.py
@@ -5,3 +5,10 @@ import cv2
 import matplotlib.pyplot as plt
 from sklearn import linear_model
 from sklearn.model_selection import train_test_split
+
+# set the main size
+standard_shape = (8, 32)
+main_size = standard_shape[0] * standard_shape[1]
+# print(main_size)
+box = np.empty((0, 4), dtype=int)
+plate_name = "plate.jpg"
