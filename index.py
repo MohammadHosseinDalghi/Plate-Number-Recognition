ef59a19e35ed697349b360a6e2192d234f3baf6a
Mohammad Hossein Dalghi <mamasi_dl@yahoo.com>
2024-10-15 21:51:10 +0330

    Add reading images from folder section

--- index.py
+++ index.py
@@ -17,3 +17,16 @@ plate_name = "plate.jpg"
 def load_img(image):
     img = cv2.resize(cv2.imread(image, 0), (standard_shape[0], standard_shape[1])).flatten()
     return img
+
+# reading images
+images_arr = np.empty((0, main_size), dtype=int)
+numbers_arr = np.empty((0), dtype=int)
+
+for i in range(1, 10):
+    files = os.listdir("dataset/" + str(i))
+    for filename in files:
+        img = load_img("dataset/" + str(i) + "/" + filename)
+        images_arr = np.append(images_arr, [img], axis=0)
+        numbers_arr = np.append(numbers_arr, i)
+
+print(numbers_arr)
