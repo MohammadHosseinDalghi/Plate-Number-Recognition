66af9b5a55515dafddeeabe57d0efcfbfc96078f
Mohammad Hossein Dalghi <mamasi_dl@yahoo.com>
2024-10-16 13:47:33 +0330

    Add some variables

--- index.py
+++ index.py
@@ -11,7 +11,7 @@ standard_shape = (8, 32)
 main_size = standard_shape[0] * standard_shape[1]
 # print(main_size)
 box = np.empty((0, 4), dtype=int)
-plate_name = "plate.jpg"
+plate_name = "plate.png"
 
 # Define load_img function
 def load_img(image):
@@ -46,8 +46,18 @@ plate_base = cv2.imread(plate_name)
 plate_base = cv2.resize(plate_base, (410, 90))
 plate = plate_base.copy()
 plate = cv2.cvtColor(plate, cv2.COLOR_BGR2GRAY)
+plate = cv2.blur(plate, (3, 3))
 _, plate = cv2.threshold(plate, 64, 255, cv2.THRESH_BINARY)
-
-# visualising images
+plate = cv2.blur(plate, (3, 3))
 plot = 90 - np.sum(plate, axis=0, keepdims=True) / 255
-plt.plot(plot[0])
+
+linethreshold = 5
+
+min_width = 15
+max_width = 50
+
+start_index = 0
+end_index = 0
+
+prev_value = 0
+final_number = ""
